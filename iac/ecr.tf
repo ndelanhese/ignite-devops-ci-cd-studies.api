1d67aa0ce7810e231f7ab0ef941cfe591d380251
ndelanhese <nnathanh3@gmail.com>
2024-06-18 07:56:12 -0300

    feat: :construction_worker: create iac files including iam and ecr service

---
+++ iac/ecr.tf
@@ -0,0 +1,12 @@
+resource "aws_ecr_repository" "delanhese_devops_ci_api" {
+  name = "delanhese_devops_ci"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    "IAC" = "True" 
+  }
+}
