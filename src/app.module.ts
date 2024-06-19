086a8879316a16a161f9af8e102bab53b44cdd09
ndelanhese <nnathanh3@gmail.com>
2024-06-19 06:19:19 -0300

    fix: :bug: change src files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    // TypeOrmModule.forRoot({
-    // 	type: "mysql",
-    // 	host: "mysql",
-    // 	port: 3308,
-    // 	username: "admin",
-    // 	password: "root",
-    // 	database: "rocketseat-db",
-    // 	entities: [],
-    // 	synchronize: true,
-    // }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
