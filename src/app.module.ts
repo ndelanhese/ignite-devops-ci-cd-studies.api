22cef0a310ecb0fc467858165407663c83d73efb
ndelanhese <nnathanh3@gmail.com>
2024-06-19 05:59:14 -0300

    fix: :bug: remove db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,21 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-	imports: [
-		TypeOrmModule.forRoot({
-			type: "mysql",
-			host: "mysql",
-			port: 3308,
-			username: "admin",
-			password: "root",
-			database: "rocketseat-db",
-			entities: [],
-			synchronize: true,
-		}),
-	],
-	controllers: [AppController],
-	providers: [AppService],
+  imports: [
+    // TypeOrmModule.forRoot({
+    // 	type: "mysql",
+    // 	host: "mysql",
+    // 	port: 3308,
+    // 	username: "admin",
+    // 	password: "root",
+    // 	database: "rocketseat-db",
+    // 	entities: [],
+    // 	synchronize: true,
+    // }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
